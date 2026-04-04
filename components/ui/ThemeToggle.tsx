9dc11cc62460b1c461099b91c612e46abd61b00a
arthurking124 <mr.arthur0124@gmail.com>
2026-04-04 13:49:55 +0800

    add sanity webhook revalidation

--- components/ui/ThemeToggle.tsx
+++ components/ui/ThemeToggle.tsx
@@ -45,16 +45,16 @@ export default function ThemeToggle() {
       title={isLight ? 'Switch to dark background' : 'Switch to light background'}
       className={
         isLight
-          ? 'fixed right-5 top-[18px] z-[120] h-[26px] w-[46px] rounded-full bg-[#d9d9d4] ring-1 ring-black/55 transition-all duration-200 ease-out hover:scale-105 md:right-8 md:top-[20px] md:h-[28px] md:w-[48px]'
-          : 'fixed right-5 top-[18px] z-[120] h-[26px] w-[46px] rounded-full bg-black ring-1 ring-white/80 transition-all duration-200 ease-out hover:scale-105 md:right-8 md:top-[20px] md:h-[28px] md:w-[48px]'
+          ? 'fixed right-3 top-[20px] z-[120] h-[22px] w-[38px] rounded-full bg-[#d9d9d4] ring-1 ring-black/55 transition-all duration-200 ease-out hover:scale-105 md:right-5 md:top-[22px] md:h-[24px] md:w-[40px]'
+          : 'fixed right-3 top-[20px] z-[120] h-[22px] w-[38px] rounded-full bg-black ring-1 ring-white/80 transition-all duration-200 ease-out hover:scale-105 md:right-5 md:top-[22px] md:h-[24px] md:w-[40px]'
       }
       style={{ pointerEvents: 'auto' }}
     >
       <span
         className={
           isLight
-            ? 'absolute left-[4px] top-1/2 h-[18px] w-[18px] -translate-y-1/2 rounded-full bg-black transition-all duration-200 ease-out md:h-[20px] md:w-[20px]'
-            : 'absolute left-[24px] top-1/2 h-[18px] w-[18px] -translate-y-1/2 rounded-full bg-white transition-all duration-200 ease-out md:left-[24px] md:h-[20px] md:w-[20px]'
+            ? 'absolute left-[3px] top-1/2 h-[16px] w-[16px] -translate-y-1/2 rounded-full bg-black transition-all duration-200 ease-out md:h-[18px] md:w-[18px]'
+            : 'absolute left-[19px] top-1/2 h-[16px] w-[16px] -translate-y-1/2 rounded-full bg-white transition-all duration-200 ease-out md:left-[19px] md:h-[18px] md:w-[18px]'
         }
       />
     </button>
